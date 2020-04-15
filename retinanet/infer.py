6291bf90a471d14cdc4c05593a8fd5188e0d2dd1
tobias teule <tobiasteule@gmail.com>
2020-04-15 17:35:48 +0200

    test

--- retinanet/infer.py
+++ retinanet/infer.py
@@ -148,6 +148,6 @@ def infer(model, path, detections_file, resize, max_size, batch_size, mixed_prec
                         writer.add_scalar('AP_Area=all_IoU=0.5', results[0],iteration)
                         writer.add_scalar('Test_2', results[1],iteration)
                         writer.add_scalar('Test_3', results[2],iteration)
-
+                    writer.close()
         else:
             print('No detections!')
