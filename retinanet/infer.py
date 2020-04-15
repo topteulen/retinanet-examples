a4b61b94debbc6853601de078b59fac653441bf3
tobias teule <tobiasteule@gmail.com>
2020-04-15 15:36:48 +0200

    test

--- retinanet/infer.py
+++ retinanet/infer.py
@@ -124,8 +124,7 @@ def infer(model, path, detections_file, resize, max_size, batch_size, mixed_prec
                     'category_id': cat
                 })
         if logdir is not None and test == True:
-            writer.add_scalar('Infer_score', detections[score])
-            writer.add_scalar('Infer_bbox', detections[bbox])
+            writer.add_scalar('Infer_score', score)
         if detections:
             # Save detections
             if detections_file and verbose: print('Writing {}...'.format(detections_file))
