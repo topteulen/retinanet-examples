d14c3c20e896c2b89a47e9633ac273dd07c70463
tobias teule <tobiasteule@gmail.com>
2020-04-15 15:00:35 +0200

    update parser

--- retinanet/main.py
+++ retinanet/main.py
@@ -57,6 +57,7 @@ def parse(args):
     parser_infer.add_argument('--max-size', metavar='max', type=int, help='maximum resizing size', default=1333)
     parser_infer.add_argument('--with-dali', help='use dali for data loading', action='store_true')
     parser_infer.add_argument('--full-precision', help='inference in full precision', action='store_true')
+    parser_infer.add_argument('--logdir', metavar='logdir', type=str, help='directory where to write logs')
 
     parser_export = subparsers.add_parser('export', help='export a model into a TensorRT engine')
     parser_export.add_argument('model', type=str, help='path to model')
